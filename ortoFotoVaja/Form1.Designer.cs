f21e1da16697f45cd41f12f4d019ff4ef872ac3d
andrazv09 <56391453+andrazv09@users.noreply.github.com>
2020-08-16 12:47:19 +0200

    First commit

---
+++ ortoFotoVaja/Form1.Designer.cs
@@ -0,0 +1,180 @@
+﻿namespace ortoFotoVaja
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.button1 = new System.Windows.Forms.Button();
+            this.openFileDialog2 = new System.Windows.Forms.OpenFileDialog();
+            this.button2 = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.button3 = new System.Windows.Forms.Button();
+            this.pictureBox3 = new System.Windows.Forms.PictureBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.button4 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(12, 12);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Slika 1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // openFileDialog2
+            // 
+            this.openFileDialog2.FileName = "openFileDialog2";
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(93, 12);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "Slika 2";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(12, 41);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(804, 710);
+            this.pictureBox1.TabIndex = 2;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.Click += new System.EventHandler(this.pictureBox1_Click);
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Location = new System.Drawing.Point(822, 41);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(758, 710);
+            this.pictureBox2.TabIndex = 3;
+            this.pictureBox2.TabStop = false;
+            this.pictureBox2.Click += new System.EventHandler(this.pictureBox2_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(174, 17);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(132, 13);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Start: RED Finish: GREEN";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(12, 1010);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 23);
+            this.button3.TabIndex = 5;
+            this.button3.Text = "Zdruzi sliki";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // pictureBox3
+            // 
+            this.pictureBox3.Location = new System.Drawing.Point(12, 41);
+            this.pictureBox3.Name = "pictureBox3";
+            this.pictureBox3.Size = new System.Drawing.Size(1568, 949);
+            this.pictureBox3.TabIndex = 6;
+            this.pictureBox3.TabStop = false;
+            this.pictureBox3.Visible = false;
+            this.pictureBox3.Click += new System.EventHandler(this.pictureBox3_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(312, 17);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(0, 13);
+            this.label2.TabIndex = 7;
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(93, 1010);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(75, 23);
+            this.button4.TabIndex = 8;
+            this.button4.Text = "button4";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1582, 1045);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.pictureBox3);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.OpenFileDialog openFileDialog2;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.PictureBox pictureBox3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button button4;
+    }
+}
+
---
+++ ortoFotoVaja/Form1.cs
@@ -0,0 +1,433 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Drawing.Drawing2D;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ortoFotoVaja
+{
+
+
+
+    public partial class Form1 : Form
+    {
+        public string path1 = "";
+        public string path2 = "";
+        public List<Point> pic1;
+        public List<Point> pic2;
+        Bitmap transformedImg;
+        Bitmap bitmap;
+        Bitmap img1;
+        List<int> newPosPix;
+
+
+        public Form1()
+        {
+            InitializeComponent();
+            pic1 = new List<Point>();
+            pic2 = new List<Point>();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            openFileDialog1.Filter = "Image files(*.jpg, *.jpeg, *.jpe, *.jfif, *.png) | *.jpg; *.jpeg; *.jpe; *.jfif; *.png";
+            DialogResult result = openFileDialog1.ShowDialog();
+            path1 = openFileDialog1.FileName;
+            Console.WriteLine(path1);
+            pictureBox1.Image = Image.FromFile(path1);
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            openFileDialog1.Filter = "Image files(*.jpg, *.jpeg, *.jpe, *.jfif, *.png) | *.jpg; *.jpeg; *.jpe; *.jfif; *.png";
+            DialogResult result = openFileDialog1.ShowDialog();
+            path2 = openFileDialog1.FileName;
+            Console.WriteLine(path2);
+            pictureBox2.Image = Image.FromFile(path2);
+        }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+            if (pic1.Count < 2)
+            {
+                MouseEventArgs me = (MouseEventArgs)e;
+
+                Point myPts = new Point(me.X, me.Y);
+                pic1.Add(myPts);
+                float cropX = me.X;
+                float cropY = me.Y;
+
+                Pen cropPen = new Pen(System.Drawing.Color.Red, 2);
+                if (pic1.Count == 2)
+                    cropPen.Color = System.Drawing.Color.Green;
+                cropPen.DashStyle = DashStyle.DashDotDot;
+                Cursor = Cursors.Cross;
+                pictureBox1.CreateGraphics().DrawEllipse(cropPen, cropX, cropY, 8, 8);
+                if (pic1.Count == 2)
+                    pictureBox1.CreateGraphics().FillEllipse(new System.Drawing.SolidBrush(System.Drawing.Color.Green), new Rectangle((int)cropX, (int)cropY, 8, 8));
+                if (pic1.Count == 2)
+                {
+                    cropPen.Color = System.Drawing.Color.Red;
+                    pictureBox1.CreateGraphics().DrawLine(cropPen, pic1[0], pic1[1]);
+                }
+            }
+
+        }
+
+        private void pictureBox2_Click(object sender, EventArgs e)
+        {
+            if (pic2.Count < 2)
+            {
+                MouseEventArgs me = (MouseEventArgs)e;
+                Point myPts = new Point(me.X, me.Y);
+                pic2.Add(myPts);
+                float cropX = me.X;
+                float cropY = me.Y;
+
+                Pen cropPen = new Pen(System.Drawing.Color.Red, 2);
+                if (pic2.Count == 2)
+                    cropPen.Color = System.Drawing.Color.Green;
+                cropPen.DashStyle = DashStyle.DashDotDot;
+                Cursor = Cursors.Cross;
+                pictureBox2.CreateGraphics().DrawEllipse(cropPen, cropX, cropY, 8, 8);
+                if (pic2.Count == 2)
+                    pictureBox2.CreateGraphics().FillEllipse(new System.Drawing.SolidBrush(System.Drawing.Color.Green), new Rectangle((int)cropX, (int)cropY, 8, 8));
+                if (pic2.Count == 2)
+                {
+                    cropPen.Color = System.Drawing.Color.Red;
+                    pictureBox2.CreateGraphics().DrawLine(cropPen, pic2[0], pic2[1]);
+                }
+            }
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+        public List<int> calculateNewPos(Bitmap img1, Bitmap img2, int imgpos1, int imgpos2, int mainImg) {
+            List<int> newPos =  new List<int>();
+            List<Point> tockeMain;
+            List<Point> tockeSec;
+            if(mainImg == 1)
+            {
+                tockeMain = pic1;
+                tockeSec = pic2;
+            }
+            else
+            {
+                tockeMain = pic2;
+                tockeSec = pic1;
+            }
+
+            int posX = imgpos1 + tockeMain[0].X;
+            int posY = imgpos2 + tockeMain[0].Y;
+
+            newPos.Add(posX - tockeSec[0].X);
+            newPos.Add(posY - tockeSec[0].Y);
+
+            System.Windows.Vector v1 = new System.Windows.Vector(tockeMain[0].X, tockeMain[0].Y);
+            System.Windows.Vector v12 = new System.Windows.Vector(tockeMain[1].X, tockeMain[1].Y);
+            System.Windows.Vector v2 = new System.Windows.Vector(tockeSec[0].X, tockeSec[0].Y);
+            System.Windows.Vector v22 = new System.Windows.Vector(tockeSec[1].X, tockeSec[1].Y);
+
+            System.Windows.Vector f1 = v22 - v2;
+            System.Windows.Vector f2 = v12 - v1;
+            newPos.Add((int)System.Windows.Vector.AngleBetween(f1, f2));
+            Console.WriteLine("Angle: " + newPos[2]);
+            //newPos.Add(Math.Atan2(tocke.Y - a.Y, b.X - a.X));
+
+            return newPos;
+        }
+
+        private void GetPointBounds(PointF[] points,
+        out float xmin, out float xmax,
+        out float ymin, out float ymax)
+        {
+            xmin = points[0].X;
+            xmax = xmin;
+            ymin = points[0].Y;
+            ymax = ymin;
+            foreach (PointF point in points)
+            {
+                if (xmin > point.X) xmin = point.X;
+                if (xmax < point.X) xmax = point.X;
+                if (ymin > point.Y) ymin = point.Y;
+                if (ymax < point.Y) ymax = point.Y;
+
+              
+
+
+            }
+
+            if (ymin < 0)
+            {
+                ymax = ymax + Math.Abs(ymin);
+                //ymin = 0;
+            }
+            else if (ymin > 0)
+            {
+                ymax = ymax - ymin;
+                //ymin = 0;
+            }
+
+            if (xmin < 0)
+            {
+                xmax = xmax + Math.Abs(xmin);
+                //xmin = 0;
+            }
+            else if (xmin > 0)
+            {
+                xmax = xmax - xmin;
+                //xmin = 0;
+            }
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            if(path1 == "" || path2 == "" || pic1.Count != 2 || pic2.Count != 2)
+            {
+                label2.Text = "Select 2 images and place points!!";
+                return;
+            }
+            //Combine pictures
+            pictureBox3.Visible = true;
+            img1 = new Bitmap(path1);
+            Bitmap img2 = new Bitmap(path2);
+            img1.MakeTransparent();
+            img2.MakeTransparent();
+            img1.SetResolution(96.0F, 96.0F);
+            img2.SetResolution(96.0F, 96.0F);
+
+
+            int mainImg;
+            //Find bigger picture and make it main
+            if(img1.Height >= img2.Height && img1.Width >= img2.Width)
+            {
+                mainImg = 1;
+            }
+            else
+            {
+                mainImg = 2;               
+            }
+
+            bitmap = new Bitmap(pictureBox3.Width, pictureBox3.Height);
+            bitmap.SetResolution(96.0F, 96.0F);
+
+
+
+            //Draw first image
+            using (var g = Graphics.FromImage(bitmap))
+            {
+                if (mainImg == 1)
+                    g.DrawImage(img1, (pictureBox3.Width - img1.Width) / 2, (pictureBox3.Height - img1.Height) / 2, img1.Width, img1.Height);
+                else
+                    g.DrawImage(img2, (pictureBox3.Width - img2.Width) / 2, (pictureBox3.Height - img2.Height) / 2, img2.Width, img2.Height);
+            }
+
+            pictureBox3.Image = bitmap;
+            //Calculate starting (X,Y) for second image
+            
+            if (mainImg == 1)
+                newPosPix = calculateNewPos(img1, img2, (pictureBox3.Width - img1.Width) / 2, (pictureBox3.Height - img1.Height) / 2, mainImg);
+            else
+                newPosPix = calculateNewPos(img2, img1, (pictureBox3.Width - img2.Width) / 2, (pictureBox3.Height - img2.Height) / 2, mainImg);
+
+
+            //Rotation
+            if (mainImg == 1)
+            {
+
+                int angle = newPosPix[2];
+                int scale = 1;
+
+                Matrix rotate_at_center = new Matrix();
+
+                Matrix rotate_at_origin = new Matrix();
+                rotate_at_origin.Rotate((float)angle);
+                PointF[] points =
+                {
+                    new PointF(0, 0),
+                    new PointF(img2.Width, 0),
+                    new PointF(img2.Width, img2.Height),
+                    new PointF(0, img2.Height),
+                };
+                rotate_at_origin.TransformPoints(points);
+                float xmin, xmax, ymin, ymax;
+                GetPointBounds(points, out xmin, out xmax,
+                    out ymin, out ymax);
+
+                PointF[] newPts =
+                {
+                    new PointF(pic2[0].X, pic2[0].Y),
+                    new PointF(pic2[1].X, pic2[1].Y),
+                };
+
+
+
+                int wid = (int)Math.Round(xmax);
+                int hgt = (int)Math.Round(ymax);
+
+                transformedImg = new Bitmap(wid, hgt);
+
+                rotate_at_center.RotateAt(angle,
+                    new PointF(wid / 2f, hgt / 2f));
+
+
+                //Calculate new points
+                Console.WriteLine("OLD POINTS: " + newPts[0].X + " " + newPts[0].Y);
+                Console.WriteLine("OLD POINTS2: " + newPts[1].X + " " + newPts[1].Y);
+                rotate_at_origin.TransformPoints(newPts);
+
+                newPts[0].X += Math.Abs(xmin);
+                newPts[0].Y += Math.Abs(ymin);
+                newPts[1].X += Math.Abs(xmin);
+                newPts[1].Y += Math.Abs(ymin);
+
+                Console.WriteLine("NEW POINTS: " + newPts[0].X + " " + newPts[0].Y);
+                Console.WriteLine("NEW POINTS2: " + newPts[1].X + " " + newPts[1].Y);
+                Console.WriteLine("MIN: " + ymin + " " + xmin);
+
+                //swap original points with new points
+                pic2.Clear();
+                pic2.Add(new Point((int)newPts[0].X, (int)newPts[0].Y));
+                pic2.Add(new Point((int)newPts[1].X, (int)newPts[1].Y));
+
+
+                using (Graphics g = Graphics.FromImage(transformedImg))
+                {
+                    // Use smooth image interpolation.
+                    g.InterpolationMode = InterpolationMode.High;
+                    //g.Clear(Color.LightBlue);
+                    g.Transform = rotate_at_center;
+
+                    int x = (wid - img2.Width) / 2;
+                    int y = (hgt - img2.Height) / 2;
+                    g.DrawImage(img2, x, y, img2.Width, img2.Height);
+                    //g.DrawImage(img1, wid - img1.Width, hgt - img1.Height, img1.Width, img1.Height);
+                }
+
+                if (mainImg == 1)
+                    newPosPix = calculateNewPos(img1, img2, (pictureBox3.Width - img1.Width) / 2, (pictureBox3.Height - img1.Height) / 2, mainImg);
+                else
+                    newPosPix = calculateNewPos(img2, img1, (pictureBox3.Width - img2.Width) / 2, (pictureBox3.Height - img2.Height) / 2, mainImg);
+                /*int angle = newPosPix[2];
+                int scale = 1;
+                transformedImg = new Bitmap(2000, 2000);
+                transformedImg.SetResolution(96.0F, 96.0F);
+
+                using (Graphics g = Graphics.FromImage(transformedImg))
+                {
+
+                    g.TranslateTransform(pic2[0].X, pic2[0].Y);
+                    g.RotateTransform(angle);
+                    g.ScaleTransform(scale, scale);
+                    g.TranslateTransform(-pic2[0].X, -pic2[0].Y);
+                    g.DrawImage(img2, 0, 0, img2.Width*scale, img2.Height*scale);
+
+                }*/
+            }
+            else
+            {
+
+                int angle = newPosPix[2];
+                int scale = 1;
+
+                Matrix rotate_at_center = new Matrix();
+                
+                Matrix rotate_at_origin = new Matrix();
+                rotate_at_origin.Rotate((float)angle);
+                PointF[] points =
+                {
+                    new PointF(0, 0),
+                    new PointF(img1.Width, 0),
+                    new PointF(img1.Width, img1.Height),
+                    new PointF(0, img1.Height),
+                };
+                rotate_at_origin.TransformPoints(points);
+                float xmin, xmax, ymin, ymax;
+                GetPointBounds(points, out xmin, out xmax,
+                    out ymin, out ymax);
+
+                PointF[] newPts =
+                {
+                    new PointF(pic1[0].X, pic1[0].Y),
+                    new PointF(pic1[1].X, pic1[1].Y),
+                };
+
+               
+
+                int wid = (int)Math.Round(xmax);
+                int hgt = (int)Math.Round(ymax);
+
+                transformedImg = new Bitmap(wid, hgt);
+
+                rotate_at_center.RotateAt(angle,
+                    new PointF(wid / 2f, hgt / 2f));
+
+                
+                //Calculate new points
+                Console.WriteLine("OLD POINTS: " + newPts[0].X + " " + newPts[0].Y);
+                Console.WriteLine("OLD POINTS2: " + newPts[1].X + " " + newPts[1].Y);
+                rotate_at_origin.TransformPoints(newPts);
+
+                newPts[0].X += Math.Abs(xmin);
+                newPts[0].Y += Math.Abs(ymin);
+                newPts[1].X += Math.Abs(xmin);
+                newPts[1].Y += Math.Abs(ymin);
+
+                Console.WriteLine("NEW POINTS: " + newPts[0].X + " " + newPts[0].Y);
+                Console.WriteLine("NEW POINTS2: " + newPts[1].X + " " + newPts[1].Y);
+                Console.WriteLine("MIN: " + ymin + " " + xmin);
+
+                //swap original points with new points
+                pic1.Clear();
+                pic1.Add(new Point((int)newPts[0].X, (int)newPts[0].Y));
+                pic1.Add(new Point((int)newPts[1].X, (int)newPts[1].Y));
+
+
+                using (Graphics g = Graphics.FromImage(transformedImg))
+                {
+                    // Use smooth image interpolation.
+                    g.InterpolationMode = InterpolationMode.High;
+                    //g.Clear(Color.LightBlue);
+                    g.Transform = rotate_at_center;
+
+                    int x = (wid - img1.Width) / 2;
+                    int y = (hgt - img1.Height) / 2;
+                    g.DrawImage(img1, x, y, img1.Width, img1.Height);
+                    //g.DrawImage(img1, wid - img1.Width, hgt - img1.Height, img1.Width, img1.Height);
+                }
+
+                if (mainImg == 1)
+                    newPosPix = calculateNewPos(img1, img2, (pictureBox3.Width - img1.Width) / 2, (pictureBox3.Height - img1.Height) / 2, mainImg);
+                else
+                    newPosPix = calculateNewPos(img2, img1, (pictureBox3.Width - img2.Width) / 2, (pictureBox3.Height - img2.Height) / 2, mainImg);
+
+            }
+
+            using (var g = Graphics.FromImage(bitmap))
+            {
+                g.DrawImage(transformedImg, newPosPix[0], newPosPix[1], transformedImg.Width, transformedImg.Height);
+                //g.DrawImage(transformedImg, 0, 0, transformedImg.Width, transformedImg.Height);
+            }
+
+        }
+
+        private void pictureBox3_Click(object sender, EventArgs e)
+        {
+            MouseEventArgs me = (MouseEventArgs)e;
+            Point myPts = new Point(me.X, me.Y);
+            Console.WriteLine(myPts.ToString());
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+           
+        }
+    }
+}
